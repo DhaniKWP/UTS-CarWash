1e025140d8858f58f191e62959061c518294e002
Dhani Kusuma Wardana Putra <dhanikusuma256@gmail.com>
2025-10-29 20:14:48 +0700

    creating model clients

---
+++ Models/modelClients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace CarWash.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string VehicleType { get; set; }
+        public string PlateNumber { get; set; }
+
+        // Navigation property
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+
+}
