41d9c96dc0987804c38e5aba8d6ed15f536e775b
Dhani Kusuma Wardana Putra <dhanikusuma256@gmail.com>
2025-10-29 19:57:27 +0700

    creating structure folder

--- obj/Debug/net8.0-windows/CarWash.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CarWash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarWash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf69c1042629f4614efbd38c5037a0df7761943e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarWash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarWash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
